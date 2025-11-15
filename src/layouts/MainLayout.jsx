7bdc158add51a570117ca6983cc098e382650166
Muhammed Elsiny <muhammedkhaled7882@gmail.com>
2025-11-15 20:59:52 +0200

    the real last

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,9 +5,9 @@ import Navbar from '../components/Navbar.jsx'
 export default function MainLayout() {
   const[isDarked,setIsDarked]=useState(false);
   return (
-    <div className={`dark:text-white ${isDarked?'dark':''}`}>
+    <div className={`dark:text-white ${localStorage.getItem('darkMode')==='true'?'dark':''}`}>
     <Navbar setIsDarked={setIsDarked} isDarked={isDarked}/>
-    <div className={`bg-gray-300 min-h-screen  dark:text-white dark:bg-black/90 py-10 -mt-5  ${isDarked?'dark':''}`}>
+    <div className={`bg-gray-300 min-h-screen  dark:text-white dark:bg-black/90 py-10 -mt-5  ${localStorage.getItem('darkMode')==='true'?'dark':''}`}>
     <div className='lg:w-4/6 md:w-5/6 mx-auto'>
     <Outlet/>
     </div>
